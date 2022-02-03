6c34c67be901416dc24777c12c138f208ba5b43f
Niatse <75042389+Niatse@users.noreply.github.com>
2022-02-03 01:09:33 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const $spinner = document.getElementById("spinner");
-let url = "https://api.coinranking.com/v2/coins?orderBy=marketCap";
-fetch(url)
+
+
+fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100")
+
   .then((response) => response.json())
   .then((data) => mostrarData(data))
   .catch((error) => console.log(error))
@@ -8,26 +10,38 @@ fetch(url)
     $spinner.style.display = "none";
   });
 
+
+
+
+
+
+
+
+
+  
 const mostrarData = (data) => {
-  const coins = data.data.coins;
+  const coins = data
 
   let body = "";
-  for (let i = 0; i < coins.length; i++) {
+  for (let i = 0; i < data.length; i++) {
     // aca estamos usando interpolacion de strings
-    var precio = data.data.coins[i].price; //Obtengo precios
+    var precio = data[i].current_price; //Obtengo precios
     var precios = new Intl.NumberFormat("de-DE", {
       style: "currency",
       currency: "USD",
     }).format(precio);
 
-    var marketCap = data.data.coins[i].marketCap;
+    var marketCap = data[i].market_cap;
     var marketCaps = new Intl.NumberFormat("de-DE", {
       style: "currency",
       currency: "USD",
       maximumSignificantDigits: 10,
     }).format(marketCap);
-
-    body += `<tr><td>${coins[i].rank}</td><td><img  src=${coins[i].iconUrl} class="img" width="50vw" height="50vh"></td><td>${coins[i].symbol}</td><td>${precios}</td><td>${marketCaps} </td></tr>`;
+    
+    body += `<tr><td>${data[i].market_cap_rank}</td><td><img  src=${data[i].image} class="img" width="50vw" height="50vh"></td><td>${data[i].name}</td><td>${precios}</td><td>${marketCaps} </td></tr>`;
   }
   document.getElementById("data").innerHTML = body;
 };
+
+
+

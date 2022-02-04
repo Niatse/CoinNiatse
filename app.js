fdffae5b294c88f65102a0b0b748fae72614e781
Niatse <75042389+Niatse@users.noreply.github.com>
2022-02-03 22:25:42 -0300

    Correciones:

--- app.js
+++ app.js
@@ -1,48 +1,40 @@
 const $spinner = document.getElementById("spinner");
 
-
-fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100")
-
-  .then((response) => response.json())
-  .then((data) => mostrarData(data))
-  .catch((error) => console.log(error))
-  .finally(() => {
+(async () => {
+  try {
+    const response = await fetch(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100"
+    );
+    const data = await response.json();
+    mostrarData(data);
+  } catch (e) {
+    console.error(e);
+  } finally {
     $spinner.style.display = "none";
-   
-  });
+  }
+})();
 
-
-
-
-
-
-
-
-
-  
 const mostrarData = (data) => {
-  const coins = data
+  const coins = data;
 
   let body = "";
   for (let i = 0; i < data.length; i++) {
     // aca estamos usando interpolacion de strings
-    var precio = data[i].current_price; //Obtengo precios
-    var precios = new Intl.NumberFormat("de-DE", {
+    let precio = data[i].current_price; //Obtengo precios
+    let precios = new Intl.NumberFormat("de-DE", {
       style: "currency",
       currency: "USD",
+      maximumSignificantDigits: 3,
     }).format(precio);
 
-    var marketCap = data[i].market_cap;
-    var marketCaps = new Intl.NumberFormat("de-DE", {
+    let marketCap = data[i].market_cap;
+    let marketCaps = new Intl.NumberFormat("de-DE", {
       style: "currency",
       currency: "USD",
       maximumSignificantDigits: 10,
     }).format(marketCap);
-    
+
     body += `<tr><td>${data[i].market_cap_rank}</td><td><img  src=${data[i].image} class="img" width="50vw" height="50vh"></td><td>${data[i].name}</td><td>${precios}</td><td>${marketCaps} </td></tr>`;
   }
   document.getElementById("data").innerHTML = body;
 };
-
-
-
